7b1baba12286c280f865862d5c032da9ca1d2f7d
seb74813 <39569227+seb74813@users.noreply.github.com>
2023-02-26 15:51:54 -0500

    Set up test code

---
+++ Notl.MuseumMap.Tests/MapTester.cs
@@ -0,0 +1,54 @@
+namespace Notl.MuseumMap.Tests
+{
+    [TestClass]
+    public class MapTester
+    {
+        [TestMethod]
+        public void MapCRUDTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void MapModificationTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ActiveMapTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void UserMapTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PoiCRUDTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PoiModificationTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void MapErrorTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void POIErrorTest()
+        {
+
+        }
+    }
+}
